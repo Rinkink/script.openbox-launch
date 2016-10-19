df89486f382ac8b0f8eb37c158393ca5cc38bbbe
Rinkink <rinkink@gmail.com>
2016-10-19 09:22:49 +0200

    Better error handling

--- addon.py
+++ addon.py
@@ -5,24 +5,30 @@ from distutils.spawn import find_executable
 import xbmcaddon
 import xbmcgui
 
-
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
 # addon_handle = int(sys.argv[1])
 
+# Logging setup
 #TODO: Log to /var or kodi user temp dir, set log level in settings
 logLevel = logging.getLevelName('DEBUG')
-#logging.basicConfig(filename='/tmp/adon.log',level=logLevel)
-logging.basicConfig(stream=sys.stdout,level=logLevel)
+fileLogger = logging.FileHandler('/tmp/adon.log')
+fileLogger.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
+logging.getLogger().addHandler(fileLogger)
+stderrLogger=logging.StreamHandler()
+stderrLogger.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
+logging.getLogger().addHandler(stderrLogger)
+logging.getLogger().setLevel(logLevel)
 
 logging.debug(addonname + ' starting up.')
 
+tempPath = xbmc.translatePath('special://temp')
+logPath = xbmc.translatePath('special://logpath')
+logging.debug('temp: [' + tempPath + ']')
+logging.debug('logpath: [' + logPath + ']')
+
 toRun = addon.getSetting('torun') # returns the string 'true' or 'false'
 stopPlayback = True
-
-# xbmcgui.Dialog().ok(addonname, line1, line2, line3)
-# steam_bin=`which steam`
-# steam_command = '-tenfoot -enableremotecontrol'
 
 openboxCommand = find_executable('kodi-openbox-runprogram')
 steamCommand = find_executable('steam')
@@ -32,7 +38,6 @@ logging.debug('steamCommand:' + steamCommand)
 openbox = '/usr/bin/kodi-openbox-runprogram'
 steam = '/usr/games/steam'
 steamArguments = [ '-tenfoot', '-enableremotecontrol']
-#steamCommand = 'kodi-openbox-runprogram /usr/games/steam -tenfoot -enableremotecontrol'
 
 all = [ openbox, steam ] + steamArguments
 
@@ -42,11 +47,15 @@ logging.debug(all)
 if stopPlayback :
     xbmc.Player().stop()
 
-s = subprocess.Popen([ openbox, steam ] + steamArguments, shell=False)
-s.communicate()
-# xbmcplugin.endOfDirectory(pluginhandle)
-xbmc.executebuiltin("ReplaceWindow(Programs,%s)" % ("plugin://"+addonID+"/"))
+logging.debug('Running command')
 
+try:
+	s = subprocess.Popen([ openbox, steam ] + steamArguments, shell=False)
+	s.communicate()
+except Exception as e:
+	logging.error(e)
+else:
+	logging.debug('Replacing window')
+	xbmc.executebuiltin("ReplaceWindow(Programs,%s)" % ("plugin://"+addonID+"/"))
 
-# xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
-# xbmcplugin.endOfDirectory(addon_handle)
+logging.debug('Done?')
